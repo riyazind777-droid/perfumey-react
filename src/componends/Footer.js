b8a698ba68465c1ecbcb45877ce8fa7a3a1fa1bf
riyazind777-droid <riyazind777@gmail.com>
2026-07-22 04:20:08 +0530

    updated project

---
+++ src/componends/Footer.js
@@ -0,0 +1,20 @@
+function Footer()
+{
+  return( <footer>
+           
+            <div class="contact">    
+                <h3>Contact</h3>
+                <p style={{marginTop: "5px"}}>+91 3478631928 <br></br>
+                    perfumy@gmail.com
+                </p>
+
+            </div>
+            
+
+           <p style={{fontSize: "small", padding:" 20px",color: "grey"}}>
+           
+           </p>
+        </footer>)
+}
+
+export default Footer
---
+++ src/componends/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (<nav>
+
+        <h1>Perfumy</h1>
+
+        <ul>
+            <li><a>  Home | </a></li>
+            <li><a>Products |</a> </li>
+            <li><a>About |</a> </li>
+            <li><a>Contact |</a> </li>
+        </ul>
+    </nav >)
+}
+
+export default Navbar
